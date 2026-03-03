ffaedaca7ae5bb5045f75f8f25ccfa014bad0b8b
BryllandPerveen <bryllatienza@gmail.com>
2026-03-03 09:26:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+AOS.init();
+
 // Countdown
 const weddingDate=new Date("April 23, 2026 15:00:00").getTime();
 
@@ -11,19 +13,34 @@ document.getElementById("minutes").innerText=Math.floor((distance%(1000*60*60))/
 document.getElementById("seconds").innerText=Math.floor((distance%(1000*60))/1000);
 },1000);
 
-// Floating Flowers
-const container=document.querySelector('.flowers');
-const icons=["🌸","🌼","🌷"];
+// Falling PNG Flowers
+const flowers=["public/flower1.png","public/flower2.png","public/flower3.png"];
+const container=document.querySelector(".flowers");
 
 function createFlower(){
-const flower=document.createElement("div");
-flower.classList.add("flower");
-flower.innerText=icons[Math.floor(Math.random()*icons.length)];
-flower.style.left=Math.random()*100+"vw";
-flower.style.fontSize=(18+Math.random()*20)+"px";
-flower.style.animationDuration=(8+Math.random()*6)+"s";
-container.appendChild(flower);
-setTimeout(()=>flower.remove(),14000);
+const img=document.createElement("img");
+img.src=flowers[Math.floor(Math.random()*flowers.length)];
+img.classList.add("flower");
+img.style.left=Math.random()*100+"vw";
+img.style.width=(18+Math.random()*20)+"px";
+img.style.animationDuration=(8+Math.random()*5)+"s";
+container.appendChild(img);
+setTimeout(()=>img.remove(),13000);
 }
 
 setInterval(createFlower,900);
+
+// Music
+const toggle=document.getElementById("musicToggle");
+const music=document.getElementById("backgroundMusic");
+const text=document.getElementById("musicText");
+
+toggle.addEventListener("click",()=>{
+if(music.paused){
+music.play();
+text.textContent="Playing Music";
+}else{
+music.pause();
+text.textContent="Play Music";
+}
+});
