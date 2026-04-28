d6788cd9f21f37809f1632f2a6105f7ccbe8c681
BryllandPerveen <bryllatienza@gmail.com>
2026-04-28 16:13:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -391,3 +391,19 @@ openBtn.addEventListener("click", () => {
   }, 1400);
 
 });
+
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach(slide => slide.classList.remove("active"));
+  slides[index].classList.add("active");
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+// auto play every 5 seconds
+setInterval(nextSlide, 5000);
